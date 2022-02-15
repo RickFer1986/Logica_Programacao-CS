43710c0569f9ef86e147e04ba261432ce9bfaff2
Ricardo Ferreira <ricardo.lazafer@gmail.com>
2022-02-14 23:09:33 -0300

    Commit - Ajuste do ambiente

---
+++ Processamento_de_Dados/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+ using System;
+ using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+/* ################### Conversão INT para DOUBLE ###################
+
+int a, b;
+double resultado;
+
+a = 5;
+b = 2;
+resultado = (double)a / b;
+
+System.Console.WriteLine(resultado.ToString("F2", CI));
+*/
+
+/* ################### Conversão de DOUBLE para INT ################### */
+
+double a;
+int b;
+a = 5.2;
+b = (int)a;
+
+System.Console.WriteLine(b.ToString("F2", CI));
---
+++ pasta_01/anotacoes.md
@@ -0,0 +1,5 @@
+Iniciando C#:
+    # dotnet new console (Rodar esse comando na pasta do projeto)
+
+Executando script:
+    # dotnet run
